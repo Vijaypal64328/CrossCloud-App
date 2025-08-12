8107f50dfb8e236f749bc8183fe6f81b4f045415
Vijaypal64328 <singhvijaypal64328@gmail.com>
2025-08-12 07:32:40 +0530

    just checked

--- server/server.js
+++ server/server.js
@@ -10,12 +10,14 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 
 const app = express();
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "http://192.168.41.26:3000",
-    "https://crosscloud-app-frontend.onrender.com"
-  ],
+  origin: process.env.NODE_ENV === 'production'
+    ? process.env.CLIENT_URL || "https://crosscloud-app-frontend.onrender.com"
+    : [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:3000",
+        "http://192.168.41.26:3000"
+      ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
